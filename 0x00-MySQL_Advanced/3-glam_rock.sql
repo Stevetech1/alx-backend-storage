199d706b97ea9d731276089f9e0389c1a3fca0ee
Stevetech1 <steveojata54@gmail.com>
2023-10-19 12:11:06 -0700

    updated

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,20 +1,6 @@
--- Create a temporary table to calculate lifespan
-CREATE TEMPORARY TABLE BandLifespan AS
-SELECT
-    band_name,
-    CASE
-        WHEN formed = 'N/A' OR formed = '' OR split = 'N/A' OR split = '' THEN NULL
-        ELSE YEAR('2022-01-01') - YEAR(CONCAT(formed, '-01-01'))
-    END AS lifespan
-FROM metal_bands;
-
--- List Glam rock bands ranked by longevity
-SELECT band_name, lifespan
-FROM BandLifespan
-WHERE band_name IN (
-    SELECT band_name
-    FROM metal_bands
-    WHERE style LIKE '%Glam rock%'
-)
-ORDER BY lifespan;
-
+-- Lists all bands with Glam rock as their main style,
+-- ranked by their longevity
+SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
