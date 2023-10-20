02e26b788eb5550a03237553bb6d67d45ee4cae4
Stevetech1 <steveojata54@gmail.com>
2023-10-19 17:08:15 -0700

    updated

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,30 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3
 """
-Caching request module
+Task: Working with Redis
 """
-import redis
-import requests
-from functools import wraps
-from typing import Callable
+import redis
+import requests
+from functools import wraps
+from typing import Callable
 
 
-def track_get_page(fn: Callable) -> Callable:
-    """ Decorator for get_page
-    """
-    @wraps(fn)
-    def wrapper(url: str) -> str:
-    """ Wrapper that:
-    - check whether a url's data is cached
-    - tracks how many times get_page is called
-    """
-        client = redis.Redis()
-        client.incr(f'count:{url}')
-        cached_page = client.get(f'{url}')
-        if cached_page:
-            return cached_page.decode('utf-8')
-        response = fn(url)
-        client.set(f'{url}', response, 10)
-            return response
-        return wrapper
+redis_store = redis.Redis()
 
 
-    @track_get_page
-def get_page(url: str) -> str:
-    """ Makes a http request to a given endpoint
-     """
-    response = requests.get(url)
-    return response.text
+def data_cacher(method: Callable) -> Callable:
+    @wraps(method)
+    def invoker(url) -> str:
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
+
+
+@data_cacher
+def get_page(url: str) -> str:
+    return requests.get(url).text
